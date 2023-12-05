183d5a9e866f007ee7ab7f0e5cc570f7644a3f9d
Troy Black <hejtroy@gmail.com>
2023-12-06 01:29:21 +0900

    Create a basic working game of rock paper scissors.

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-const answers = ["rock","paper","scissors"];
+// Below is the code for the computer's random selection.
+const rockPaperSciccors = ["rock","paper","scissors"];
 
 function getComputerChoice() {
-    const randomChoice = Math.floor(Math.random() * answers.length);
+    const randomChoice = Math.floor(Math.random() * rockPaperSciccors.length);
 
-    const choice = answers[randomChoice];
+    const choice = rockPaperSciccors[randomChoice];
 
     return choice;
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
+
+// Below is the code for the user's selection. It's set to lower case to match the original array.
+let getUserChoice = prompt("Make your choice! Rock, paper or sciccors!", "");
+
+let userChoice = getUserChoice.toLocaleLowerCase();
+
+// console.log(userChoice);
+function game() {
+if(getComputerChoice() === "rock" && userChoice === "scissors") {
+    return "You lose! Rock beats scissors...";
+} else if(getComputerChoice() === "paper" && userChoice === "rock") {
+    return "You lose! Paper beats scissors...";
+} else if(getComputerChoice() === "scisors" && userChoice === "paper") {
+    return "You lose! Scissors beat paper...";
+} else if(getComputerChoice() === "rock" && userChoice === "paper") {
+    return "You win! Paper beats rock! ^^";
+} else if(getComputerChoice() === "paper" && userChoice === "scissors") {
+    return "You win! Scisors beat paper! ^^";
+} else if(getComputerChoice() === "scissors" && userChoice === "rock") {
+    return "You win! Rock beats scissors! ^^";
+} else {
+    return "Tie! Play again.";
+    }
+}
+console.log(game());
